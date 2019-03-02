dac30a1c1a659ec195e86f07fb39b87d06d3498a
yamamoto <yy.ek.166.154@gmail.com>
2019-03-02 14:15:07 +0900

    add new file

---
+++ build/precache-manifest.fd127e52688f68d5006ef9318afa4f58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79a525daefe4de317dae",
+    "url": "/static/css/main.0ee1e851.chunk.css"
+  },
+  {
+    "revision": "79a525daefe4de317dae",
+    "url": "/static/js/main.79a525da.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "750aab7a01926a8f8bf5",
+    "url": "/static/js/2.750aab7a.chunk.js"
+  },
+  {
+    "revision": "202b96550dba30d8b15c921d7ca378e9",
+    "url": "/index.html"
+  }
+];
